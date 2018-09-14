5bea6b2076f58cc522f9a782b4f173d90563fe58
allen.chang <allennchang@gmail.com>
2018-09-14 10:49:59 +0800

    物流參數檢核調整GoodsName,SenderName,ReceiverName

--- Example/Ruby/create_b2c_c2c.rb
+++ Example/Ruby/create_b2c_c2c.rb
@@ -1,37 +1,36 @@
 require 'ecpay_logistics'
 
-class YOURCONTROLLER < ApplicationController
 
-  def Create_B2CandC2C
+
     ## 參數值為[PLEASE MODIFY]者，請在每次測試時給予獨特值
 	## 預設參數為全帶，欄位值會因條件不同而有所不同 ##
     b2candc2c_param = {
       'MerchantTradeNo' => '', #請帶20碼uid, ex: f0a0d7e9fae1bb72bc93, 為aiocheckout時所產生的
-      'MerchantTradeDate' => '', #請帶交易時間, ex: 2017/05/17 16:23:45, 為aiocheckout時所產生的
-      'LogisticsType' => 'CVS' 
-      'LogisticsSubType' => 'FAMI',  
-      'GoodsAmount' => '100',
-      'CollectionAmount' => '',
+      'MerchantTradeDate' => '2017/05/17 16:23:45', #請帶交易時間, ex: 2017/05/17 16:23:45, 為aiocheckout時所產生的
+      'LogisticsType' => 'CVS',
+      'LogisticsSubType' => 'UNIMART',
+      'GoodsAmount' => '20000',
+      'CollectionAmount' => '20000',
 	  'IsCollection' => '',
-	  'GoodsName' => '',
-      'SenderName' => '綠界科技',  
-      'SenderPhone' => ''     
+	  'GoodsName' => 'test',
+      'SenderName' => '綠界科技',
+      'SenderPhone' => '',
       'SenderCellPhone' => '0912345678',  
-      'ReceiverName' => '綠界科技',
+      'ReceiverName' => '綠界!@#',
       'ReceiverPhone' => '',
 	  'ReceiverCellPhone' => '0912345678',
 	  'ReceiverEmail' => '',
       'TradeDesc' => '',  
       'ServerReplyURL' => 'http://192.168.0.1/ReceiverServerReply', #物流狀況會通知到此URL
       'ClientReplyURL' => '',  
-      'LogisticsC2CReplyURL' => '',
+      'LogisticsC2CReplyURL' => 'https://www.ecpay.com.tw',
       'Remark' => '',
 	  'PlatformID' => '',
-	  'ReceiverStoreID' => '', #請帶收件人門市代號
+	  'ReceiverStoreID' => '991182', #請帶收件人門市代號
 	  'ReturnStoreID' => ''
     }
 
-    create = ECpayLogistics::CreatClient.new
+    create = ECpayLogistics::CreateClient.new
     res = create.create(b2candc2c_param)
-    render :text => res
-  end
+    puts res
+
