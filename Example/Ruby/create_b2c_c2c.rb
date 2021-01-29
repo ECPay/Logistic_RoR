724bd09a412051c6d9941d6cd477461064814c90
adela.chang <adela.chang@ecpay.com.tw>
2021-01-29 13:21:20 +0800

    新增OK超商 #107266

--- Example/Ruby/create_b2c_c2c.rb
+++ Example/Ruby/create_b2c_c2c.rb
@@ -5,18 +5,18 @@ require 'ecpay_logistics'
     ## 參數值為[PLEASE MODIFY]者，請在每次測試時給予獨特值
 	## 預設參數為全帶，欄位值會因條件不同而有所不同 ##
     b2candc2c_param = {
-      'MerchantTradeNo' => '', #請帶20碼uid, ex: f0a0d7e9fae1bb72bc93, 為aiocheckout時所產生的
-      'MerchantTradeDate' => '2017/05/17 16:23:45', #請帶交易時間, ex: 2017/05/17 16:23:45, 為aiocheckout時所產生的
+      'MerchantTradeNo' => '210127tst1215615156', #請帶20碼uid, ex: f0a0d7e9fae1bb72bc93, 為aiocheckout時所產生的
+      'MerchantTradeDate' => '2021/01/27 16:23:45', #請帶交易時間, ex: 2017/05/17 16:23:45, 為aiocheckout時所產生的
       'LogisticsType' => 'CVS',
-      'LogisticsSubType' => 'UNIMART',
-      'GoodsAmount' => '20000',
-      'CollectionAmount' => '20000',
+      'LogisticsSubType' => 'OKMARTC2C',
+      'GoodsAmount' => '1000',
+      'CollectionAmount' => '',
 	  'IsCollection' => '',
 	  'GoodsName' => 'test',
       'SenderName' => '綠界科技',
       'SenderPhone' => '',
       'SenderCellPhone' => '0912345678',  
-      'ReceiverName' => '綠界!@#',
+      'ReceiverName' => '綠界科技',
       'ReceiverPhone' => '',
 	  'ReceiverCellPhone' => '0912345678',
 	  'ReceiverEmail' => '',
@@ -26,7 +26,7 @@ require 'ecpay_logistics'
       'LogisticsC2CReplyURL' => 'https://www.ecpay.com.tw',
       'Remark' => '',
 	  'PlatformID' => '',
-	  'ReceiverStoreID' => '991182', #請帶收件人門市代號
+	  'ReceiverStoreID' => '001328', #請帶收件人門市代號
 	  'ReturnStoreID' => ''
     }
 
